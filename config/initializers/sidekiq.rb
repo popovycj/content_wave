ad4597f4c3967e74d9f8d0c77f6eab40db4bc283
popovycj <marian.popovych.ua@gmail.com>
2023-10-06 16:48:53 +0300

    Change initializers/sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,15 @@
+url = ENV["REDIS_URL"] || "redis://localhost:6379/0"
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = {
+    url: url,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = {
+      url: url,
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
